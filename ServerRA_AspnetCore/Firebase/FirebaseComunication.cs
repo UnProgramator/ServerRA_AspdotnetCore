f11e0ca62e9e9c8e1e6475b3584f8926567d0d3e
Pescaru Alexandru <apescaru11@yahoo.com>
2022-05-07 18:39:15 +0300

    intermediate commit

---
+++ ServerRA_AspnetCore/Firebase/FirebaseComunication.cs
@@ -0,0 +1,9 @@
+﻿namespace ServerRA_AspnetCore.Firebase
+{
+    public class FirebaseComunication
+    {
+
+        public const string database_url = "jIvVHLmeBCPtpBcmx9yGtxHBeMH2";
+        public const string web_api_key = "AIzaSyD4L5mu4RnToo3JL-Hz3L_UzR-AuwyVgKI";
+    }
+}
--- ServerRA_AspnetCore/Program.cs
+++ ServerRA_AspnetCore/Program.cs
@@ -1,4 +1,10 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+
 var builder = WebApplication.CreateBuilder(args);
+
+//admin@firebase.com   admin1
+
+//firebase real-time db https://computercompany-64270-default-rtdb.europe-west1.firebasedatabase.app
 
 // Add services to the container.
 
@@ -6,6 +12,19 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+                                                                                        {
+                                                                                            options.Authority = "https.https://securetoken.google.com/1:449726311356:web:6b06b1126f37de8975d9b9";
+                                                                                            options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters {
+                                                                                                ValidateIssuer = true,
+                                                                                                ValidIssuer = "https.https://securetoken.google.com/1:449726311356:web:6b06b1126f37de8975d9b9",
+                                                                                                ValidateAudience = true,
+                                                                                                ValidAudience = "1:449726311356:web:6b06b1126f37de8975d9b9",
+                                                                                                ValidateLifetime = true
+                                                                                            };
+                                                                                        }
+);
 
 var app = builder.Build();
 
@@ -17,6 +36,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
